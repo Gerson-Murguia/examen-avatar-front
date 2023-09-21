6d7d33240a41a5dc15375a4a018373423fa77631
Gerson Murguia Montes <aldmurguia1@gmail.com>
2023-09-21 17:30:38 -0500

    Agregadas routes para formularios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmComponent } from './film/film.component';
+import { FilmDetailComponent } from './film-detail/film-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: '/films', pathMatch: 'full'
+},
+{
+  path: 'films',
+  component: FilmComponent
+},
+{
+  path: 'film/:id',
+  component: FilmDetailComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
