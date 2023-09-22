18675d360cc72eec186279961838e192a1b4b3b8
Gerson Murguia Montes <aldmurguia1@gmail.com>
2023-09-21 19:46:26 -0500

    Añadidas llamadas a getFilmById y getCharactersByFilmId

--- src/app/service/film.service.ts
+++ src/app/service/film.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environment/environment.prod';
 import { FilmResponse } from '../model/FilmResponse';
+import { Films } from '../model/Films';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,14 @@ export class FilmService {
   constructor(private http:HttpClient) { }
 
   public getFilms(): any{
-    return this.http.get(`${this.host}/films`);
+    return this.http.get(`${this.host}/film`);
+  }
+
+  public getFilmById(id:number): any{
+    return this.http.get(`${this.host}/film/${id}`);
+  }
+
+  public getCharactersByFilmId(filmId:number):any{
+    return this.http.get(`${this.host}/characters/${filmId}`);
   }
 }
