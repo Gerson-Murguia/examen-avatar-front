f5512d1218bd80a64bf6719ed57b9bde6bb89961
Gerson Murguia Montes <aldmurguia1@gmail.com>
2023-09-21 20:29:55 -0500

    Agregado almacenamiento en LocalStorage de peticiones repetidas para eliminar demoras

--- src/app/film-detail/film-detail.component.ts
+++ src/app/film-detail/film-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FilmService } from '../service/film.service';
 
 @Component({
@@ -13,26 +13,46 @@ export class FilmDetailComponent implements OnInit {
   characters:[];
   isLoading: boolean = true;
   isCharactersLoading: boolean = true;
-  constructor(private route: ActivatedRoute, private filmService:FilmService){
+  constructor(private router:Router, private route: ActivatedRoute, private filmService:FilmService){
 
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.route.params.subscribe( (params: any) => {
+    const charactersData = localStorage.getItem('charactersDatos');
+    const detailsData = localStorage.getItem('detailsDatos');
+    if (charactersData && detailsData) {
+      this.characters = JSON.parse(charactersData);
+      this.film=JSON.parse(detailsData)
+      this.isCharactersLoading=false;
+      this.isLoading=false;
+    } else {
+      this.cargarDetalles();
+    }
+
+
+
+   
+  }
+
+  regresar() {
+    this.router.navigate(['/']);
+  }
+
+  cargarDetalles(){
+     this.route.params.subscribe( (params: any) => {
       this.filmId =+params['id'];
       //cargar detalles de la pelicula con el service
       this.filmService.getFilmById(this.filmId).subscribe((data:[])=>{this.film = data
+        localStorage.setItem('detailsDatos', JSON.stringify(data));
         this.isLoading=false;
       });
       //cargar nombres de personajes
       this.filmService.getCharactersByFilmId(this.filmId).subscribe((data:[])=>{
         this.characters = data;
+        localStorage.setItem('charactersDatos', JSON.stringify(data));
         this.isCharactersLoading=false;
       });
-  
     });
   }
-
-
 }
