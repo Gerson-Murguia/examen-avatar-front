793b2f1025912eff2a1dbe44073fbe1a5ce039b4
Gerson Murguia Montes <aldmurguia1@gmail.com>
2023-09-21 19:45:54 -0500

    Acabado detalle de film y llamadas a pis de film y personajes

--- src/app/film-detail/film-detail.component.ts
+++ src/app/film-detail/film-detail.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FilmService } from '../service/film.service';
 
 @Component({
   selector: 'app-film-detail',
   templateUrl: './film-detail.component.html',
   styleUrls: ['./film-detail.component.css']
 })
-export class FilmDetailComponent {
+export class FilmDetailComponent implements OnInit {
+  filmId: number;
+  film:any;
+  characters:[];
+  isLoading: boolean = true;
+  isCharactersLoading: boolean = true;
+  constructor(private route: ActivatedRoute, private filmService:FilmService){
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.route.params.subscribe( (params: any) => {
+      this.filmId =+params['id'];
+      //cargar detalles de la pelicula con el service
+      this.filmService.getFilmById(this.filmId).subscribe((data:[])=>{this.film = data
+        this.isLoading=false;
+      });
+      //cargar nombres de personajes
+      this.filmService.getCharactersByFilmId(this.filmId).subscribe((data:[])=>{
+        this.characters = data;
+      });
+        this.isCharactersLoading=false;
+    });
+  }
+
 
 }
